446202ca9e5c955a3a5ff23e3721779cfb22f386
Vikas Saini <vikassaini883@gmail.com>
2026-06-15 13:32:33 -0400

    fixed linting errors

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -137,12 +137,10 @@ class TestAccountService(TestCase):
         data = resp.get_json()
         self.assertEqual(data["name"], account.name)
 
-
     def test_get_account_not_found(self):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
-
 
     def test_get_account_list(self):
         """It should Get a list of Accounts"""
@@ -151,7 +149,6 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(len(data), 5)
-
 
     def test_update_account(self):
         """It should Update an existing Account"""
@@ -174,12 +171,10 @@ class TestAccountService(TestCase):
         resp = self.client.delete(f"{BASE_URL}/{account.id}")
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
 
-
     def test_method_not_allowed(self):
         """It should not allow an illegal method call"""
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-
 
     def test_security_headers(self):
         """It should return security headers"""
@@ -193,7 +188,6 @@ class TestAccountService(TestCase):
         }
         for key, value in headers.items():
             self.assertEqual(response.headers.get(key), value)
-            
             
     def test_cors_security(self):
         """It should return a CORS header"""
